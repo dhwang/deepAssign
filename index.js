a25736b2a50577ec93bfbd5ecc841974665a1cd8
Darren Hwang <dhwang101@gmail.com>
2019-12-23 17:56:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.export = function() {
 
   const deepAssign = (obj, obj2) => {
   Object.keys(obj2).forEach(key => {
-    if (typeof obj[key] === 'object') obj[key] = mapAssign(obj[key], obj2[key]);
+    if (typeof obj[key] === 'object') obj[key] = deepAssign(obj[key], obj2[key]);
     else obj[key] = obj2[key]
   });
   return obj;
